42594702a7cb11f49104b4da2a7edec7d82ede7e
George Soderholm <georgesoderholm@gmail.com>
2020-08-27 13:15:35 -0600

    Add four test methods

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,6 +1,40 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/cell'
+require './lib/ship'
 
 class CellTest < Minitest::Test
-end 
+  def test_it_exists
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+
+  def test_has_coordinate
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+  end
+
+  def test_it_has_a_ship
+    cell = Cell.new("B4")
+
+    assert_equal nil, cell.ship
+  end
+
+  def test_it_is_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  def test_place_ship
+    # skip
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+
+    assert_equal cruiser, cell.ship
+  end
+end
