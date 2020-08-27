5fd0b768e4884804a482b45c82f8c97d97de268c
George Soderholm <georgesoderholm@gmail.com>
2020-08-27 13:49:43 -0600

    Finish place_ship method. Add fired_upon? method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,17 +1,21 @@
 class Cell
-  attr_reader :coordinate, :ship, :empty
+  attr_reader :coordinate, :ship, :empty, :fired_upon
   def initialize(coordinate)
     @coordinate = coordinate
-    @ship = []
+    @ship = nil
     @empty = true
+    @fired_upon = false
   end
 
   def empty?
-    @empty
+    @empty = @ship == nil
   end
 
   def place_ship(ship_name)
-    # ship ship_name
-    # empty? #false
+    @ship = ship_name
+  end
+
+  def fired_upon?
+    @fired_upon
   end
 end
