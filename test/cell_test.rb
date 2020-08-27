11e425491a962bf56cd29c5baccfc8560fd80435
George Soderholm <georgesoderholm@gmail.com>
2020-08-27 13:50:29 -0600

    Add three test methods

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -10,6 +10,7 @@ class CellTest < Minitest::Test
     assert_instance_of Cell, cell
   end
 
+
   def test_has_coordinate
     cell = Cell.new("B4")
 
@@ -36,5 +37,30 @@ class CellTest < Minitest::Test
     cell.place_ship(cruiser)
 
     assert_equal cruiser, cell.ship
+  end
+
+  def test_that_if_ship_placed_cell_not_empty
+    cell = Cell.new("B4")
+    submarine = Ship.new("Submarine", 2)
+
+    cell.place_ship(submarine)
+
+    assert_equal false, cell.empty?
+    assert_equal submarine, cell.ship
+  end
+
+  def test_it_has_attributes
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+    assert_equal nil, cell.ship
+    assert_equal true, cell.empty
+  end
+
+  def test_it_begins_not_fired_upon
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal false, cell.fired_upon?
   end
 end
