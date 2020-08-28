a14661d0a296c72b51568376dff79cf5505a0d7c
Megan Campbell <megan.campbell26@yahoo.com>
2020-08-27 18:14:27 -0600

    Add board class

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board
+  attr_reader :cells
+  def initialize
+    @cells = {}
+  end
+
+  def cells
+    board_size = 4
+
+    rows = [1..(board_size)]
+    columns = []
+    alphabet = ["A".."Z"]
+  end
+
+end
