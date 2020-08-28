39069548479f7a5627eb4bd1dfe85ef256a61209
Megan Campbell <megan.campbell26@yahoo.com>
2020-08-28 17:20:14 -0600

    Add 3 tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -21,5 +21,24 @@ class BoardTest < Minitest::Test
     board = Board.new
 
     assert_equal 16, board.cells.length
+    refute_equal 15, board.cells.length
+    refute_equal 17, board.cells.length
+  end
+
+  def test_the_values_are_cells
+    board = Board.new
+
+    board.cells
+
+    assert_equal Cell, board.cells.values[1].class
+  end
+
+  def test_cell_is_unique
+      board = Board.new
+
+      board.cells
+
+      assert_equal 16, board.cells.values.uniq.length
+      assert_equal 16, board.cells.keys.uniq.length
   end
 end
