99bb0b987a18cb2c32f04cf9c5b6f126625a5505
George Soderholm <georgesoderholm@gmail.com>
2020-08-29 11:58:04 -0600

    Add nine test methods to aide the valid_placement method

--- test/board_test.rb
+++ test/board_test.rb
@@ -70,6 +70,19 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_placement?(submarine, ["A1", "A2","A3"])
   end
 
+  def test_validate_lengths_are_equal
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    board.cells
+
+    assert_equal true, board.coordinate_equal?(submarine, ["A1", "A2"])
+    assert_equal true, board.coordinate_equal?(cruiser, ["A1", "A2","A3"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2","A3"])
+  end
+
   def test_is_a_valid_placement
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
@@ -81,13 +94,78 @@ class BoardTest < Minitest::Test
     assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
   end
 
-  def test_coordinates_are_not_consecutive_numbers
+  def test_coordinates_are_consecutive_numbers_or_not
+    board = Board.new
+    board.cells
+
+    assert_equal true, board.consecutive_numbers?(["C1", "C2", "C3"])
+    assert_equal false, board.consecutive_numbers?(["A3", "B3", "C3"])
+  end
+
+  def test_coordinates_are_consecutive_letters_or_not
+    board = Board.new
+    board.cells
+
+    assert_equal false, board.consecutive_letters?(["C1", "C2", "C3"])
+    assert_equal true, board.consecutive_letters?(["A3", "B3", "C3"])
+  end
+
+  def test_all_letters_same_method_works_both_ways
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     board.cells
 
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2","A4"])
+    assert_equal false, board.all_letters_same?(["A1", "A2","B3"])
+    assert_equal true, board.all_letters_same?(["A1", "A2","A3"])
+  end
+
+  def test_all_numbers_same_method_works_both_ways
+    board = Board.new
+    board.cells
+
+    assert_equal true, board.all_numbers_same?(["A2", "B2","C2"])
+    assert_equal false, board.all_numbers_same?(["A2", "B2","C3"])
+  end
+
+  def test_it_can_find_all_digits
+    board = Board.new
+    board.cells
+
+    assert_equal [2, 3, 4], board.find_digits(["D2", "D3", "D4"])
+    assert_equal [2, 2, 2], board.find_digits(["B2", "C2", "D2"])
+  end
+
+  def test_it_can_find_all_letters
+    board = Board.new
+    board.cells
+
+    assert_equal [68, 68, 68], board.find_letters(["D2", "D3", "D4"])
+    assert_equal [66, 67, 68], board.find_letters(["B2", "C2", "D2"])
+  end
+
+  def test_it_can_make_a_standard_ord_number_array
+    board = Board.new
+    board.cells
+
+    assert_equal [65, 66, 67, 68], board.make_letters_ord_numbers
+    # assert_equal [65, 66, 67, 68, 69, 70], board.make_letters_ord_numbers
+  end
+
+  def test_consecutive_numbers_true_and_all_letters_same
+    board = Board.new
+    board.cells
+
+    assert_equal true, board.valid_numbers?(["D2", "D3", "D4"])
+    assert_equal false, board.valid_numbers?(["B4", "C4", "D4"])
+  end
+
+  def test_consecutive_letters_true_and_all_numbers_same
+    board = Board.new
+    board.cells
+
+    assert_equal false, board.valid_letters?(["D2", "D3", "D4"])
+    assert_equal true, board.valid_letters?(["B4", "C4", "D4"])
   end
 end
