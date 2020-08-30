e24fae9685e22f288c8118047ccee6927723e763
George Soderholm <georgesoderholm@gmail.com>
2020-08-30 14:27:32 -0600

    Change idea for testing computer class

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -14,31 +14,31 @@ class ComputerTest < Minitest::Test
     assert_instance_of Computer, computer
   end
 
-  def test_it_can_place_a_cruiser
-    # skip
-    board = Board.new
-    computer = Computer.new
-    cruiser = Ship.new("Cruiser", 3)
-
-    computer.place_cruiser
-    # cell_count = board.cells.values.count do |cell|
-      # require 'pry';binding.pry
-      # !(board.cells[cell].ship.nil?)
-    # end
-#ship is not nil
-    # assert_equal 3, cell_count
-    assert_equal true, board.valid_placement?(cruiser, cruiser_spots)
-  end
-
-  def test_it_can_place_a_submarine
-    skip
-    board = Board.new
-    computer = Computer.new
-    submarine = Submarine("Submarine", 2)
-
-    computer.place_cruiser
-
-    assert_equal
-  end
+#   def test_it_can_place_a_cruiser
+#     # skip
+#     board = Board.new
+#     computer = Computer.new
+#     cruiser = Ship.new("Cruiser", 3)
+#
+#     computer.place_cruiser
+#     # cell_count = board.cells.values.count do |cell|
+#       # require 'pry';binding.pry
+#       # !(board.cells[cell].ship.nil?)
+#     # end
+# #ship is not nil
+#     # assert_equal 3, cell_count
+#     assert_equal true, board.valid_placement?(cruiser, cruiser_spots)
+#   end
+#
+#   def test_it_can_place_a_submarine
+#     skip
+#     board = Board.new
+#     computer = Computer.new
+#     submarine = Submarine("Submarine", 2)
+#
+#     computer.place_cruiser
+#
+#     assert_equal
+#   end
 
 end
