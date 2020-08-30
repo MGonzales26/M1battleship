0806130e735cfe7da28bf7959a960c63fb43f19f
George Soderholm <georgesoderholm@gmail.com>
2020-08-30 13:17:37 -0600

    Edit cruiser method

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -5,6 +5,7 @@ require './lib/ship'
 require './lib/board'
 require './lib/turn'
 require './lib/game'
+require './lib/computer'
 
 class ComputerTest < Minitest::Test
   def test_it_exists
@@ -14,19 +15,30 @@ class ComputerTest < Minitest::Test
   end
 
   def test_it_can_place_a_cruiser
-    skip
+    # skip
     board = Board.new
     computer = Computer.new
     cruiser = Ship.new("Cruiser", 3)
 
     computer.place_cruiser
-
-    assert_equal 
+    # cell_count = board.cells.values.count do |cell|
+      # require 'pry';binding.pry
+      # !(board.cells[cell].ship.nil?)
+    # end
+#ship is not nil
+    # assert_equal 3, cell_count
+    assert_equal true, board.valid_placement?(cruiser, cruiser_spots)
   end
 
   def test_it_can_place_a_submarine
     skip
+    board = Board.new
     computer = Computer.new
+    submarine = Submarine("Submarine", 2)
+
+    computer.place_cruiser
+
+    assert_equal
   end
 
 end
