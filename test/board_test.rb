07645579eac2e92f867d80844f2f8816ccaa1a97
George Soderholm <georgesoderholm@gmail.com>
2020-09-02 15:25:20 -0600

    Edit the unit tests of valid placement

--- test/board_test.rb
+++ test/board_test.rb
@@ -6,21 +6,21 @@ require './lib/board'
 
 class BoardTest < Minitest::Test
   def test_it_exists
-    # skip
+    skip
     board = Board.new
 
     assert_instance_of Board, board
   end
 
   def test_it_is_a_hash
-    # skip
+    skip
     board = Board.new
 
     assert_equal Hash, board.cells.class
   end
 
   def test_it_has_16_cells
-    # skip
+    skip
     board = Board.new
 
     assert_equal 16, board.cells.length
@@ -29,14 +29,14 @@ class BoardTest < Minitest::Test
   end
 
   def test_the_values_are_cells
-    # skip
+    skip
     board = Board.new
 
     assert_equal Cell, board.cells.values[1].class
   end
 
   def test_cell_is_unique
-      # skip
+      skip
     board = Board.new
 
     assert_equal 16, board.cells.values.uniq.length
@@ -44,14 +44,14 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_is_a_valid_coordinate
-    # skip
+    skip
     board = Board.new
 
     assert_equal true, board.valid_coordinate?("A1")
   end
 
   def test_is_not_valid_and_outside_board
-    # skip
+    skip
     board = Board.new
 
     assert_equal false, board.valid_coordinate?("Q1")
@@ -59,7 +59,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_is_not_a_valid_placement_lengths_not_equal
-    # skip
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -69,7 +69,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_validate_lengths_are_equal
-    # skip
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -81,7 +81,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_is_a_valid_placement
-    # skip
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -91,7 +91,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_coordinates_are_consecutive_numbers_or_not
-    # skip
+    skip
     board = Board.new
 
     assert_equal true, board.consecutive_numbers?(["C1", "C2", "C3"])
@@ -99,7 +99,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_coordinates_are_consecutive_letters_or_not
-    # skip
+    skip
     board = Board.new
 
     assert_equal false, board.consecutive_letters?(["C1", "C2", "C3"])
@@ -107,7 +107,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_all_letters_same_method_works_both_ways
-    # skip
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -117,7 +117,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_all_numbers_same_method_works_both_ways
-    # skip
+    skip
     board = Board.new
 
     assert_equal true, board.all_numbers_same?(["A2", "B2","C2"])
@@ -125,7 +125,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_find_all_digits
-    # skip
+    skip
     board = Board.new
 
     assert_equal [2, 3, 4], board.find_digits(["D2", "D3", "D4"])
@@ -133,7 +133,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_find_all_letters
-    # skip
+    skip
     board = Board.new
 
     assert_equal [68, 68, 68], board.find_letters(["D2", "D3", "D4"])
@@ -141,7 +141,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_make_a_standard_ord_number_array
-    # skip
+    skip
     board = Board.new
 
     assert_equal [65, 66, 67, 68], board.make_letters_ord_numbers
@@ -149,7 +149,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_consecutive_numbers_true_and_all_letters_same
-    # skip
+    skip
     board = Board.new
 
     assert_equal true, board.valid_numbers?(["D2", "D3", "D4"])
@@ -157,7 +157,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_consecutive_letters_true_and_all_numbers_same
-    # skip
+    skip
     board = Board.new
 
     assert_equal false, board.valid_letters?(["D2", "D3", "D4"])
@@ -165,7 +165,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_place_method
-    # skip
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
 
@@ -181,7 +181,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_has_the_same_ship_for_all_coordinates_given
-    # skip
+    skip
     board = Board.new
     submarine = Ship.new("Submarine", 2)
     board.place(submarine, ["A1", "A2"])
@@ -205,37 +205,47 @@ class BoardTest < Minitest::Test
     assert_equal submarine, cell_1.ship
   end
 
-  def test_it_a_cell_cannot_have_overlapping_ships
+  def test_a_cell_cannot_have_overlapping_ships
     # skip
     board = Board.new
     submarine = Ship.new("Submarine", 2)
     cruiser = Ship.new("Cruiser", 3)
-    # assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
+    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
     board.place(cruiser, ["A1", "A2", "A3"])
-    board.place(submarine, ["A1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
 
-    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
+    assert_equal false, board.not_overlapping?(["A1", "B1"])
   end
 
-  def test_it_can_find_overlap_in_different_coordinates
-    # skip
+  def test_it_can_return_true_when_none_are_overlapping
     board = Board.new
     submarine = Ship.new("Submarine", 2)
     cruiser = Ship.new("Cruiser", 3)
-    board.place(cruiser, ["D1", "D2", "D3"])
-    board.place(submarine, ["D2", "D3"])
 
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    assert_equal false, board.not_overlapping?(["A1", "B1"])
+  end
+  def test_it_can_find_overlap_in_different_coordinates
+    skip
+    board = Board.new
+    submarine = Ship.new("Submarine", 2)
+    cruiser = Ship.new("Cruiser", 3)
+    
+    board.place(cruiser, ["D1", "D2", "D3"])
+
+    assert_equal false, board.not_overlapping?(["D2", "D3"])
     assert_equal false, board.valid_placement?(submarine, ["D2", "D3"])
   end
 
   def test_it_can_render_the_current_board
-    # skip
+    skip
     board = Board.new
 
     assert_equal " 1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
   end
   def test_it_can_render_the_current_board_not_showing_ship
-    # skip
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["D1", "D2", "D3"])
@@ -244,10 +254,21 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_render_a_board_with_a_ship
-    #
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["D1", "D2", "D3"])
+
+    assert_equal " 1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD S S S . \n", board.render(true)
+  end
+
+  def test_it_can_render_a_missed_shot
+    skip
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["D1", "D2", "D3"])
+
+
 
     assert_equal " 1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD S S S . \n", board.render(true)
   end
